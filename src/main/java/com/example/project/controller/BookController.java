0fc840002b6714f0bfc99b7a0ea14d769b6ab55b
Stalker1234567 <117269356+Stalker1234567@users.noreply.github.com>
2023-04-01 16:47:02 +0200

    Update-Library-Application-Branch-1

--- src/main/java/com/example/project/controller/BookController.java
+++ src/main/java/com/example/project/controller/BookController.java
@@ -1,19 +1,74 @@
 package com.example.project.controller;
 
+import com.example.project.dao.impl.BookDAOImpl;
+import com.example.project.dao.impl.UserDAOImpl;
+import com.example.project.model.Book;
+import com.example.project.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/book")
 public class BookController {
+
+    private final BookDAOImpl bookDAO;
+
+    @Autowired
+    public BookController(BookDAOImpl bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
     @GetMapping()
-    public String listBooks() {
-        return "books";
+    public String index(Model model) {
+        model.addAttribute("book", bookDAO.index());
+        return "book/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "book/show";
     }
 
     @GetMapping("/new")
-    public String createNewBook() {
-        return "createBook";
+    public String newPerson(@ModelAttribute("book") Book book) {
+        return "book/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") @Valid Book book,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "book/new";
+
+        bookDAO.save(book);
+        return "redirect:/book";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "book/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "book/edit";
+
+        bookDAO.update(id, book);
+        return "redirect:/book";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        bookDAO.delete(id);
+        return "redirect:/book";
     }
 }
